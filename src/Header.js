088ec44361e536981f3ed2a8258e1f8e82398c7f
digitalprashant07 <digitalprashant07@outlook.com>
2023-01-12 12:05:43 +0530

    new ui

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './App.css'
 
 function Header() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">SearchBar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,23 +16,27 @@ function Header() {
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">About Us</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Services
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Web development</a></li>
+            <li><a className="dropdown-item" href="#">Web designing</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="#">Search engine optimization</a></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <a className="nav-link">Projects</a>
         </li>
       </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="text" placeholder="Search"/>
+        <button class="btn btn-primary" type="button">Search</button>
+      </form>
     </div>
   </div>
 </nav>
